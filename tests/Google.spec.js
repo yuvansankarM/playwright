20899898627000e8fe17de7389bcdfe688c2b74f
yuvansankar <osskiruba@gmail.com>
2025-02-27 19:39:04 +0530

    google

--- tests/Google.spec.js
+++ tests/Google.spec.js
@@ -3,5 +3,6 @@ test ('locator',async({page})=>{
 await page.goto("https://www.google.com/");
 await expect(page).toHaveURL("https://www.google.com/");
 await expect(page).toHaveTitle("Google");
+await page.waitfortimeout(3000);
 // 
 }) 
