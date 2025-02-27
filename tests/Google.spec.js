b6084f73f668b7f471433f019f9700548cdf08b4
yuvansankar <osskiruba@gmail.com>
2025-02-27 19:27:09 +0530

    Add google

---
+++ tests/Google.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+test ('locator',async({page})=>{
+await page.goto("https://www.google.com/");
+await expect(page).toHaveURL("https://www.google.com/");
+await expect(page).toHaveTitle("Google");
+// 
+}) 
